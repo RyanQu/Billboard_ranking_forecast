16fbbd3afc8053db7b5dc631e5993cd3db62619b
RyanQu <ryanqu94@sina.com>
2017-04-24 14:13:16 -0400

    data & crawler

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -4,7 +4,9 @@
 #Ver:0.9
 
 # -*- coding: utf-8 -*-
-
+import time
+import random
+import datetime
 import requests
 import sys
 from bs4 import BeautifulSoup
@@ -24,60 +26,84 @@ header={
 def get_html(url):
     print "Get html"
     html=requests.get(url,headers=header).text
+    time.sleep(5*random.random())
     return html
 
-url = 'http://www.billboard.com/charts/hot-100'
-html = get_html(url)
-soup = BeautifulSoup(html,"html.parser")
-#cur = soup(class_="chart-row")
+cha=datetime.timedelta(days=1)
+boardday=[]
+for n in xrange(0,200):
+    now=datetime.datetime.now()-cha
+    delta=datetime.timedelta(days=7*n)
+    dates=now-delta
+    past_n_weeks=dates.strftime('%Y-%m-%d')
+    boardday.append(past_n_weeks)
 
-song_cur = soup(class_="chart-row__song")
-song=[]
-for sp in song_cur:
-    song.append(sp.text.strip())
+print boardday
 
-rank_cur = soup(class_="chart-row__current-week")
-rank=[]
-for sp in rank_cur:
-    rank.append(int(sp.text.strip()))
+f=open('data.csv','w')
+print >> f, '"rank","song_name","last_week","peak_position","wks_on_rank","artist_name","date"'
+f.close()
 
-stats_cur = soup(class_="chart-row__stats")
-lastrank=[]
-peak=[]
-weeks=[]
-for sp in stats_cur:
-    stats = sp.find_all(class_="chart-row__value")
-    count = 0
-    for i in stats:
-        if (count == 0):
-            lastrank.append(i.text)
-        if (count == 1):
-            peak.append(i.text)
-        if (count == 2):
-            weeks.append(i.text)
-        count = count + 1
+chart_id=0
+for day in boardday:
+    chart_id=chart_id+1
+    print chart_id
+    print day
+    url = 'http://www.billboard.com/charts/hot-100/'+str(day)
+    html = get_html(url)
+    soup = BeautifulSoup(html,"html.parser")
+    #cur = soup(class_="chart-row")
+
+    song_cur = soup(class_="chart-row__song")
+    song=[]
+    for sp in song_cur:
+        song.append(sp.text.strip().replace(',',' '))
+
+    rank_cur = soup(class_="chart-row__current-week")
+    rank=[]
+    for sp in rank_cur:
+        rank.append(int(sp.text.strip()))
+
+    stats_cur = soup(class_="chart-row__stats")
+    lastrank=[]
+    peak=[]
+    weeks=[]
+    for sp in stats_cur:
+        stats = sp.find_all(class_="chart-row__value")
+        count = 0
+        for i in stats:
+            if (count == 0):
+                lastrank.append(i.text)
+            if (count == 1):
+                peak.append(i.text)
+            if (count == 2):
+                weeks.append(i.text)
+            count = count + 1
 
 #print lastrank, peak, weeks
 
-artist_cur = soup(class_="chart-row__artist")
-artist=[]
-for sp in artist_cur:
-    artist.append(sp.text.strip().replace(',',' '))
+    artist_cur = soup(class_="chart-row__artist")
+    artist=[]
+    for sp in artist_cur:
+        artist.append(sp.text.strip().replace(',',' '))
 
-data = zip(rank, song, lastrank, peak, weeks, artist)
+    day_list=[]
+    for i in range(100):
+        day_list.append(day)
 
-f=open('data.csv','w')
-print >> f, '"rank","song_name","last_week","peak_position","wks_on_rank","artist_name"'
-for line in data:
-    count=0
-    for ch in line:
-        count+=1
-        if count==6:
-            print >> f,ch
-        else:
-            print >> f,ch,',',
+    data = zip(rank, song, lastrank, peak, weeks, artist,day_list)
 
-f.close()
+    f=open('data.csv','a')
+    for line in data:
+        line_count=0
+        for ch in line:
+            line_count+=1
+            if line_count==7:
+                print >> f,ch
+            else:
+                print >> f,ch,',',
+
+    f.close()
 
 # f=open('test.html','w')
 # print >> f,get_html(url)
